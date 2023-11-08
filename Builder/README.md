12e3becf32ed44ecc98f0afa941ae8ac5b25e918
AbiFreytes <69989593+AbiFreytes@users.noreply.github.com>
2023-11-08 08:19:11 -0300

    Delete Builder directory

--- Builder/README.md
+++
@@ -1,2 +0,0 @@
-# Builder
-## Ejecutar el archivo "run.py" para correr el codigo de ejemplo.
--- Builder/run.py
+++
@@ -1,5 +0,0 @@
-from main import Client
-
-if __name__ == '__main__':
-    client = Client()
-    client.run()
